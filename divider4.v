8f3400b0a94c39ee1ca0f62b08f3be9da4728be8
Maris Usis <mau13@case.edu>
2024-12-03 01:18:47 -0800

    Update

---
+++ divider4.v
@@ -0,0 +1,21 @@
+module divider4 (
+    input clock,
+    input reset,
+    input [4-1:0] dividend,
+    input [4-1:0] divisor,
+    output [4-1:0] Q,
+    output [4-1:0] A,
+    output done
+);
+
+  divider #(.N(4)) D (
+    clock,
+    reset,
+    dividend,
+    divisor,
+    Q,
+    A,
+    done
+  );
+
+endmodule;
